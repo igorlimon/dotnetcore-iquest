c98a1008e7196143722e1a34ad28d0e92eb0ca8a
limon <igorz.jigan@gmail.com>
2019-01-20 21:04:47 +0200

    Email tag helper

---
+++ Module 3/TagHelpers/TagHelpers/EmailTagHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace TagHelpers.TagHelpers
+{
+    public class CompanyEmailTagHelper : TagHelper
+    {
+        private const string EmailDomain = "iquest.ro";
+
+        public string MailTo { get; set; }
+
+        public override async void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";
+            var content = await output.GetChildContentAsync();
+            var target = content.GetContent() + "@" + EmailDomain;
+            output.Attributes.SetAttribute("href", "mailto:" + target);
+            output.Content.SetContent(target);
+        }
+    }
+}
